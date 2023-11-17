6269dbc27fe7c2f70ea54a239e0f169aac8b386a
Theint Nandar Su <theintnandarsu246@gmail.com>
2023-11-17 12:57:03 +0700

    Add general healthcare tips

---
+++ healthcare_tips.pl
@@ -0,0 +1,18 @@
+% Healthcare Tips based on PotentialIllnessIllness
+
+healthcare_tips(flu, '1. Rest and drink plenty of fluids. \n2. Take over-the-counter medications (e.g., acetaminophen or ibuprofen) to alleviate symptoms. \n3. Stay home and avoid spreading the virus to others.\n\n').
+
+healthcare_tips(cold, '1. Get plenty of rest. \n2. Stay hydrated. \n3. Use over-the-counter cold medications (e.g., decongestants, cough syrups) to relieve symptoms.\n\n').
+
+healthcare_tips(migraine, '1. Find a quiet, dark place to rest. \n2. Apply a cold or warm compress to the head. \n3. Take prescribed migraine medications if available (e.g., triptans).\n\n').
+
+healthcare_tips(pneumonia, '1. Follow your doctor\'s prescribed treatment plan. \n2. Take all medications as directed (e.g., antibiotics, cough medicine). \n3. Get plenty of rest and stay hydrated. \n4. Use a humidifier to ease breathing.\n\n').
+
+healthcare_tips(heart_disease, '1. Follow a heart-healthy diet. \n2. Engage in regular exercise. \n3. Take prescribed medications consistently (e.g., beta-blockers, statins). \n4. Manage stress through relaxation techniques. \n5. Quit smoking if applicable.\n\n').
+
+healthcare_tips(diabetes, '1. Monitor blood sugar levels regularly. \n2. Follow a balanced diet and exercise regularly. \n3. Take medications as prescribed (e.g., insulin, metformin). \n4. Manage stress and get adequate sleep.\n\n').
+
+healthcare_tips(asthma, '1. Identify and avoid asthma triggers. \n2. Take prescribed medications, including inhalers (e.g., albuterol), as directed. \n3. Create an asthma action plan with your healthcare provider.\n\n').
+
+healthcare_tips(general_skin_rash, '1. Keep the affected area clean and dry. \n2. Avoid scratching the rash. \n3. Apply over-the-counter creams or ointments (e.g., hydrocortisone). \n4. If the rash persists, consult with a dermatologist.\n\n').
+
