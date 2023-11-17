32a6220ed66b68b970d36784f28251317bd3ea79
Hannah Ebenezar <91257701+AnnaEbenezar@users.noreply.github.com>
2023-11-17 15:49:13 +0700

    Update mh_symptom.pl

--- mh_symptom.pl
+++ mh_symptom.pl
@@ -1,22 +1,26 @@
 % Include main program and knowledge base
-:- consult('main_program.pl').
+%:- consult('main_program.pl').
 :- consult('kb_mh_symptom.pl').
 
 % Rules for diagnosis
-:- dynamic(has_symptom/2).
+:- dynamic(symptoms_list/1).
+symptoms_list([]).
+
 diagnose_mental_health(Illness) :-
     write('What symptoms are you experiencing? (separated by space): '),
     read_line_to_string(user_input, Symptoms),
     split_string(Symptoms, " ", " ", SymptomsList),
     check_mental_health_symptoms(SymptomsList, Illness).
 
-check_mental_health_symptoms([], unknown).
+check_mental_health_symptoms([], unknown) :-
+    write('I\'m sorry, I couldn\'t identify any specific illness based on the provided symptoms. Please consult a healthcare professional for a more accurate diagnosis.'), nl.
+    
 check_mental_health_symptoms([Symptom | Rest], Illness) :-
     atom_string(TrimmedSymptom, Symptom),
     findall(PotentialIllness, mental_health_symptom(TrimmedSymptom, PotentialIllness), PotentialIllnesses),
     count_matching_symptoms(Rest, PotentialIllnesses, MatchingSymptomsCount),
     length(PotentialIllnesses, TotalSymptoms),
-    (MatchingSymptomsCount >= TotalSymptoms / 2 ->
+    (MatchingSymptomsCount >= 2 ->
         write('Suggested mental health disorder according to your symptoms: '), write(PotentialIllnesses), nl,
         Illness = PotentialIllnesses; 
         check_mental_health_symptoms(Rest, Illness)
