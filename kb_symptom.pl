78be3e4f48ce4e5c8fffaa208fac02726b57a3f5
Theint Nandar Su <theintnandarsu246@gmail.com>
2023-11-17 22:46:37 +0700

    Change strings

--- kb_symptom.pl
+++ kb_symptom.pl
@@ -1,48 +1,48 @@
 %flu
-symptom(fever, flu).
-symptom(cough, flu).
-symptom(runny_nose, flu).
-symptom(sore_throat, flu).
+symptom('fever', flu).
+symptom('cough', flu).
+symptom('runny nose', flu).
+symptom('sore throat', flu).
 
 %cold
-symptom(fever, cold).
-symptom(cough, cold).
-symptom(runny_nose, cold).
-symptom(sore_throat, cold).
+symptom('fever', cold).
+symptom('cough', cold).
+symptom('runny nose', cold).
+symptom('sore throat', cold).
 
 %migraine
-symptom(headache, migraine).
-symptom(sensitivity_to_light, migraine).
-symptom(nausea, migraine).
+symptom('headache', migraine).
+symptom('sensitivity to light', migraine).
+symptom('nausea', migraine).
 
 %pnuemonia
-symptom(cough, pneumonia).
-symptom(fever, pneumonia).
-symptom(shortness_of_breath, pneumonia).
-symptom(chest_pain, pneumonia).
+symptom('cough', pneumonia).
+symptom('fever', pneumonia).
+symptom('shortness of breath', pneumonia).
+symptom('chest pain', pneumonia).
 
 %heart disease
-symptom(chest_pain, heart_disease).
-symptom(shortness_of_breath, heart_disease).
-symptom(fatigue, heart_disease).
-symptom(dizziness, heart_disease).
-symptom(irregular_heartbeat, heart_disease).
+symptom('chest pain', heart_disease).
+symptom('shortness of breath', heart_disease).
+symptom('fatigue', heart_disease).
+symptom('dizziness', heart_disease).
+symptom('irregular heartbeat', heart_disease).
 
 %diabetes
-symptom(increased_thirst, diabetes).
-symptom(frequent_urination, diabetes).
-symptom(blurred_vision, diabetes).
-symptom(fatigue, diabetes).
+symptom('increased_thirst', diabetes).
+symptom('frequent_urination', diabetes).
+symptom('blurred_vision', diabetes).
+symptom('fatigue', diabetes).
 
 %asthma
-symptom(shortness_of_breath, asthma).
-symptom(wheezing, asthma).
-symptom(chest_tightness, asthma).
-symptom(cough, asthma).
+symptom('shortness of breath', asthma).
+symptom('wheezing', asthma).
+symptom('chest tightness', asthma).
+symptom('cough', asthma).
 
 %skin rash
-symptom(skin_rash, general_skin_rash).
-symptom(redness, general_skin_rash).
-symptom(itchiness, general_skin_rash).
-symptom(swelling, general_skin_rash).
+symptom('skin rash', general_skin_rash).
+symptom('redness', general_skin_rash).
+symptom('itchiness', general_skin_rash).
+symptom('swelling', general_skin_rash).
 
