0e7480f3770c46748db73d5b45cf2602459da460
Hannah Ebenezar <91257701+AnnaEbenezar@users.noreply.github.com>
2023-11-19 18:12:18 +0700

    Update mental_health_screening.pl
    giving up :anguished:

--- mental_health_screening.pl
+++ mental_health_screening.pl
@@ -17,18 +17,31 @@ screening(anxiety, [
 
 % Function to start the mental health screening
 start_mental_health_screening :-
-    write('We care about your mental health. Let\'s assess how you are feeling.'), nl,
+    write('Which mental health screening would you like to take?'), nl,
     write('Please answer the following questions on a scale of 0 to 4, where 0 is "Not at all" and 4 is "Nearly every day".'), nl,
-    nl,
-    % Start the first screening
-    start_screening(depression).
+    write_screening_options,
+    read_line_to_string(user_input, SelectedScreeningWithNewline), % Read input with newline
+    atom_string(SelectedScreeningAtom, SelectedScreeningWithNewline),
+    atom_string(SelectedScreening, SelectedScreeningAtom), % Convert to atom to remove newline
+    start_selected_screening(SelectedScreening).
 
-% Function to start a specific screening
-start_screening(Screening) :-
-    screening(Screening, Questions),
+% Function to display screening options
+write_screening_options :-
+    findall(Screening, screening(Screening, _), Screenings),
+    write_screening_options(Screenings, 1).
+
+write_screening_options([], _).
+write_screening_options([Screening | Rest], Index) :-
+    format('~d. ~w~n', [Index, Screening]),
+    NextIndex is Index + 1,
+    write_screening_options(Rest, NextIndex).
+
+% Function to start the selected screening
+start_selected_screening(SelectedScreening) :-
+    screening(SelectedScreening, Questions),
     ask_questions(Questions, 0, Total),
     calculate_percentage(Total, Percentage, Questions),
-    interpret_results(Screening, Percentage).
+    interpret_results(SelectedScreening, Percentage).
 
 % Function to ask screening questions
 ask_questions([], Total, Total).
@@ -55,12 +68,21 @@ interpret_results(Screening, Percentage) :-
         write('Your score indicates a high likelihood of issues. It is recommended to seek professional care.'), nl;
         write('Your score is within a normal range. However, if you have concerns, consider consulting with a professional.'), nl
     ),
-    ask_for_further_screenings.
+    nl,
+    ask_for_further_screenings. % Prompt user for another screening
 
 ask_for_further_screenings :-
     write('Would you like to take another mental health screening? (yes/no): '),
-    read(Response),
-    (Response = 'yes' ->
-        start_mental_health_screening; % Start another screening
-        write('Thank you for using MediGuide. If you have any other questions, feel free to ask.'), nl
-    ).
+    flush_output(user), % Ensure the prompt is displayed
+    read_line_to_string(user_input, ResponseWithNewline), % Read input with newline
+    atom_string(Response, ResponseWithNewline), % Convert to atom to remove newline
+    process_response(Response).
+
+process_response(yes) :-
+    start_mental_health_screening. % Start another screening
+process_response(no) :-
+    write('Thank you for using MediGuide. If you have any other questions, feel free to ask.'), nl.
+process_response(_) :-
+    write('I did not understand that. Please enter "yes" or "no".'), nl,
+    ask_for_further_screenings.
+    
